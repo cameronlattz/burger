8fd104107702a3612794a7dad845be7acfc623f4
Cameron Lattz <camerolattz@gmail.com>
2019-09-26 13:20:29 -0500

    fixing heroku connection

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Eat-Da-Burger
 
 ### TODO:
-* Recolor
 * Deploy to Heroku
 * Comment code
 * Write readme
-* Add to portfolio
--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
+let localhost = {
   host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
   database: "burgers_db"
-});
+};
+const connection = mysql.createConnection(process.env.JAWSDB_URL || localhost);
 
 connection.connect(function(err) {
   if (err) {
